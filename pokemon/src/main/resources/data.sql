1a0d945ecc6578f369bbbb5fdbf19086981211ab
kmell <kmell@protonmail.com>
2024-06-19 23:02:01 -0500

    Pod 153: Synchronize pod data

--- pokemon/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into pokemon(name, type) values ('Arbok', 'Poison');
-insert into pokemon(name, type) values ('Beedrill', 'Bug');
-insert into pokemon(name, type) values ('Cloyster', 'Water');
-insert into pokemon(name, type) values ('Ditto', 'Normal');
-insert into pokemon(name, type) values ('Exeggutor', 'Grass');
-insert into pokemon(name, type) values ('Flareon', 'Fire');
--- pokemon/src/main/resources/schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS pokemon(
-    id int auto_increment,
-    name varchar(255) NOT NULL,
-    type varchar(255) NOT NULL
-);
