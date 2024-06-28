c4f8950b45711757607885bb3ae780ab8ba50f6b
kmell <kmell@protonmail.com>
2024-06-27 22:29:26 -0500

    Pod 153: Synchronize pod data

--- learn-spring/learn-spring-module1-upgraded/real-world-project/src/test/java/com/baeldung/ls/persistence/repository/impl/ProjectRepositoryImplUnitTest.java
+++ learn-spring/learn-spring-module1-upgraded/real-world-project/src/test/java/com/baeldung/ls/persistence/repository/impl/ProjectRepositoryImplUnitTest.java
@@ -1,8 +1,13 @@
 package com.baeldung.ls.persistence.repository.impl;
 
+import com.baeldung.ls.persistence.model.Project;
+import com.baeldung.ls.persistence.repository.IProjectRepository;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ProjectRepositoryImplUnitTest {
 
@@ -10,4 +15,16 @@ class ProjectRepositoryImplUnitTest {
     void findById_givenDummyId_returnsEmpty() {
         assertFalse(new ProjectRepositoryImpl().findById(123L).isPresent());
     }
+
+    @Test
+    void findById_givenValidId_returnsNonEmpty() {
+
+        Project project = new Project("Test Drive", LocalDate.now());
+        IProjectRepository repository = new ProjectRepositoryImpl();
+        repository.save(project);
+
+        assertTrue(repository.findById(project.getId()).isPresent());
+
+    }
+
 }
