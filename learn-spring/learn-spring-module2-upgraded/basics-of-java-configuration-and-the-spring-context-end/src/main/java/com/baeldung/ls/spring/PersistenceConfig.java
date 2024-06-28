3665954eac8e14fd01c7f378027c75bffed9bf46
kmell <kmell@protonmail.com>
2024-06-27 23:54:59 -0500

    Pod 153: Synchronize pod data

--- learn-spring/learn-spring-module2-upgraded/basics-of-java-configuration-and-the-spring-context-end/src/main/java/com/baeldung/ls/spring/PersistenceConfig.java
+++ learn-spring/learn-spring-module2-upgraded/basics-of-java-configuration-and-the-spring-context-end/src/main/java/com/baeldung/ls/spring/PersistenceConfig.java
@@ -1,15 +1,24 @@
 package com.baeldung.ls.spring;
 
+import com.baeldung.ls.persistence.repository.IProjectRepository;
+import com.baeldung.ls.persistence.repository.impl.ProjectRepositoryImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.baeldung.ls.persistence.repository.impl.ProjectRepositoryImpl;
 
 @Configuration
 public class PersistenceConfig {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(PersistenceConfig.class);
+
+    public PersistenceConfig() {
+        LOGGER.info("Persistence configuration loaded");
+    }
+
     @Bean
-    public ProjectRepositoryImpl projectRepository() {
+    public IProjectRepository projectRepository() {
+        LOGGER.info("Creating project repository");
         return new ProjectRepositoryImpl();
     }
 
